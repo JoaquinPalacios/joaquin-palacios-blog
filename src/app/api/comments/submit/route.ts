4a7db7167242f7719c85ccb571cbba9212746ad2
Joaquin <joa_palacios@hotmail.com>
2024-03-08 05:16:24 +1000

    supabase logic commented in order to be able to build and deploy to vercel

--- src/app/api/comments/submit/route.ts
+++ src/app/api/comments/submit/route.ts
@@ -1,26 +1,26 @@
-import { supabase } from "@/lib/supabase/server";
+// import { supabase } from "@/lib/supabase/server";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const body = await req.json();
+  // const body = await req.json();
 
-  const { post_id, email, comment, nickname, uid } = body;
+  // const { post_id, email, comment, nickname, uid } = body;
 
-  const { data, error } = await supabase
-    .from("comments")
-    .insert({
-      post_id,
-      email,
-      nickname,
-      payload: comment,
-    })
-    .select("id");
+  // const { data, error } = await supabase
+  //   .from("comments")
+  //   .insert({
+  //     post_id,
+  //     email,
+  //     nickname,
+  //     payload: comment,
+  //   })
+  //   .select("id");
 
-  if (error) {
-    return new NextResponse(JSON.stringify({ error: error.message }), {
-      status: 500,
-    });
-  }
+  // if (error) {
+  //   return new NextResponse(JSON.stringify({ error: error.message }), {
+  //     status: 500,
+  //   });
+  // }
 
   return new NextResponse(JSON.stringify({ success: true }), {
     status: 200,
--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -8,61 +8,86 @@ export function Comments({ id, uid }: { id: string; uid: string }) {
   const [nickname, setNickname] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setLoading(true);
-    console.log("test");
+  // const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   setLoading(true);
+  //   console.log("test");
 
-    await fetch(`/api/comments/submit`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        post_id: id,
-        nickname,
-        email,
-        comment,
-        uid,
-      }),
-    })
-      .then((response) => {
-        console.log(response);
-        // if (response.ok) {
-        setLoading(false);
-        setComment("");
-        setEmail("");
-        setNickname("");
-        // }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
+  //   await fetch(`/api/comments/submit`, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       post_id: id,
+  //       nickname,
+  //       email,
+  //       comment,
+  //       uid,
+  //     }),
+  //   })
+  //     .then((response) => {
+  //       console.log(response);
+  //       // if (response.ok) {
+  //       setLoading(false);
+  //       setComment("");
+  //       setEmail("");
+  //       setNickname("");
+  //       // }
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // };
 
   return (
     <div className="comments">
       <h3>Share your thoughts</h3>
-      <form onSubmit={onSubmit}>
+      <form>
+        {/* <form onSubmit={onSubmit}> */}
         <div>
           <label htmlFor="comment" className="mb-2 mt-6 text-md block">
             Comment
           </label>
-          <textarea id="comment" onChange={(e) => setComment(e.target.value)} placeholder="Your comment" className="w-full border p-4" value={comment} />
+          <textarea
+            id="comment"
+            onChange={(e) => setComment(e.target.value)}
+            placeholder="Your comment"
+            className="w-full border p-4"
+            value={comment}
+          />
         </div>
         <div>
           <label htmlFor="email" className="mb-2 mt-6 text-md block">
             Email
           </label>
-          <input id="email" onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Your email" className="w-full border p-4" value={email} />
+          <input
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="Your email"
+            className="w-full border p-4"
+            value={email}
+          />
         </div>
         <div>
           <label htmlFor="nickname" className="mb-2 mt-6 text-md block">
             Nickname
           </label>
-          <input id="nickname" onChange={(e) => setNickname(e.target.value)} type="text" placeholder="Your nickname" className="w-full border p-4" value={nickname} />
+          <input
+            id="nickname"
+            onChange={(e) => setNickname(e.target.value)}
+            type="text"
+            placeholder="Your nickname"
+            className="w-full border p-4"
+            value={nickname}
+          />
         </div>
-        <button className="p-4 rounded-lg bg-slate-700 text-white mt-6 disabled:opacity-50 disabled:cursor-not-allowed" type="submit" disabled={loading}>
+        <button
+          className="p-4 rounded-lg bg-slate-700 text-white mt-6 disabled:opacity-50 disabled:cursor-not-allowed"
+          type="submit"
+          disabled={loading}
+        >
           {loading ? "Loading..." : "Send comment"}
         </button>
       </form>
